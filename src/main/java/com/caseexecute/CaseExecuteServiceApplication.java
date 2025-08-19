352ba0ab507e4a9fca968f5c3131c82341fd2fb5
waibozie <waibozie@outlook.com>
2025-08-19 15:12:41 +0800

    删除无用数据库配置

--- src/main/java/com/caseexecute/CaseExecuteServiceApplication.java
+++ src/main/java/com/caseexecute/CaseExecuteServiceApplication.java
@@ -1,6 +1,5 @@
 package com.caseexecute;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,7 +10,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @since 2024-01-01
  */
 @SpringBootApplication
-@MapperScan("com.caseexecute.mapper")
 public class CaseExecuteServiceApplication {
 
     public static void main(String[] args) {
